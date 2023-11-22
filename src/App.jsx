577d5431208e3518e48177d60b22fd0cf049a444
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-11-22 15:19:42 +0330

    create router Page and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-
+import { useRoutes } from "react-router-dom"
+import routes from './routes'
 
 function App() {
+  const router = useRoutes(routes)
   return (
-    <div>app</div>
+    <>
+    
+    {router}
+    </>
   )
 }
 
