bf46e187a0eb10d0c8dbff596ef8819502ead3ee
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-11-29 15:35:29 +0330

    change folfer HomeTrust

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,7 +1,7 @@
 import HomeBanner from "../../components/template/home/HomeBanner/HomeBanner"
 import HomeProduct from "../../components/template/home/HomeProduct/HomeProduct"
 import ProductPack from "../../components/template/home/ProductPack/ProductPack"
-import HomeTrust from "../../components/template/home/homeTrust/homeTrust"
+import HomeTrust from "../../components/module/HomeTrust/HomeTrust"
 import HomeNews from "../../components/template/home/HomeNews/HomeNews"
 
 function Home() {
