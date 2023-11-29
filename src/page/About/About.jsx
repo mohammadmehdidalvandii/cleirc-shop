641c0669b6e3f705d6c036307c5849385834b3f5
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-11-29 15:36:36 +0330

    add Section Home trust to page-about

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -1,11 +1,13 @@
 import NavStatus from '../../components/module/NavStatus/NavStatus'
 import AboutTitleBanner from '../../components/template/about/AboutTitleBanner/AboutTitleBanner'
+import HomeTrust from '../../components/module/HomeTrust/HomeTrust'
 
 function About() {
   return (
     <>
       <NavStatus status='درباه ما'/>
       <AboutTitleBanner/>
+      <HomeTrust/>
     </>
   )
 }
