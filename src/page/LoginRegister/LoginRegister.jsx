9fb39d584d31e0fbd8c5c8116b9f037bf770d01d
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-11-29 15:57:27 +0330

    create grid page loginRegister

--- src/page/LoginRegister/LoginRegister.jsx
+++ src/page/LoginRegister/LoginRegister.jsx
@@ -4,6 +4,17 @@ function LoginRegister() {
   return (
     <>
         <NavStatus status='احراز هویت'/>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                        this is col
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    this is col
+                </div>
+            </div>
+        </div>
         <br /><br /><br /><br />
     </>
   )
