089a3f84124630b6ef08ba34255de5b52d1190ee
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-05 16:47:20 +0330

    logic counts

--- src/components/module/BasketCard/BasketCard.jsx
+++ src/components/module/BasketCard/BasketCard.jsx
@@ -6,20 +6,25 @@ function BasketCard() {
     const productData = useContext(productContext)
   
 
-    const [count ,SetCount] = useState(1)
-    const handlerBtnMinus =()=>{
-        if(count === 1){
-            SetCount(count)
+    const [counts ,SetCounts] = useState(productData.userBasket.map(product=>product.count === 1))
+
+    const handlerBtnMinus =(index)=>{
+        let newCounts = [...counts]
+        if(newCounts[index]  === 1){
+         newCounts[index] = +newCounts[index]
         }else{
-            SetCount(count -1)
+            newCounts[index] = newCounts[index] -1
         }
+        SetCounts(newCounts)
     }
-const handlerBtnPlus = ()=>{
-    SetCount(count + 1)
+const handlerBtnPlus = (index)=>{
+    let newCounts = [...counts]
+ newCounts[index] = newCounts[index] + 1
+ SetCounts(newCounts)
 }
   return (
         <>
-            {productData.userBasket.map(product=>(
+            {productData.userBasket.map((product,index)=>(
                 
                       <div className="basketCard" key={product.id}>
                       <ul className="basketCard-items">
@@ -31,13 +36,13 @@ const handlerBtnPlus = ()=>{
                           </li>
                           <li className="basketCard-item">
                               <div className="basketCard-countProduct">
-                                  <button className="basketCard-countProduct-minus basketCard-btnCount" onClick={handlerBtnMinus}>-</button>
-                                  <span className="basketCard-countProduct-number">{count}</span>
-                                  <button className="basketCard-countProduct-plus basketCard-btnCount" onClick={handlerBtnPlus}>+</button>
+                                  <button className="basketCard-countProduct-minus basketCard-btnCount" onClick={()=>handlerBtnMinus(index)}>-</button>
+                                  <span className="basketCard-countProduct-number">{counts[index] || 1}</span>
+                                  <button className="basketCard-countProduct-plus basketCard-btnCount" onClick={()=>handlerBtnPlus(index)}>+</button>
                               </div>
                           </li>
                           <li className="basketCard-item">
-                              <span className="basketCard-price">{product.price * count.toLocaleString() } تومان</span> 
+                              <span className="basketCard-price">{product.price * counts[index] || product.price } تومان</span> 
                           </li>
                           <li className="basketCard-item">
                               <span className="basketCard-DeleteBtn">
