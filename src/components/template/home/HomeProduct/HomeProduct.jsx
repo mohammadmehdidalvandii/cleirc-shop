3feca53b664cb930103558eaefb0dafdecee0c7e
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-10 15:24:48 +0330

    add breakpoints swiper

--- src/components/template/home/HomeProduct/HomeProduct.jsx
+++ src/components/template/home/HomeProduct/HomeProduct.jsx
@@ -36,6 +36,28 @@ function HomeProduct() {
             pagination={{
               dynamicBullets: true,
             }}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+            // 1100: {
+            //   slidesPerView: 3,
+            //   spaceBetween: 50,
+            // },
+          }}
             effect="Flip"
             modules={[Pagination]}
             className="mySwiper"
